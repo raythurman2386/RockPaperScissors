ff4613c73d764a9470d57f19a1b732f638afc1fa
Raymond Thurman <36676341+raythurman2386@users.noreply.github.com>
2019-01-29 14:37:41 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,103 +1,25 @@
-let winHead = document.querySelector('h3')
-let playerScoreHead = document.querySelector('#score1')
-let compScoreHead = document.querySelector('#score2')
-var button = document.querySelectorAll('#buttons')
+    let roundCount = 1;
+    let playerScore = 0;
+    let computerScore = 0;
+    let pScore = document.querySelector('.player-score');
+    let cScore = document.querySelector('.computer-score');
+    let winner = document.querySelector('.winner')
 
-button.forEach((button) => {
-    button.addEventListener('click', (e) => {
-        console.log(button.className);
-    });
-});
-
-function computerPlay() {
-    choice = Math.random();
-    if (choice <= 0.33) {
-        return 'rock';
-    } else if (choice <= 0.66) {
-        return 'paper';
-    } else {
-        return 'scissors';
-    }
-}
-
-function playerChoice() {
-    result = prompt('Choose Rock, paper, or scissors: ');
-    return result = result.toLowerCase();
-    // alert(playerChoice)
-};
+    function playRound(playerSelection, computerSelection) {
+        let playerChoice = playerSelection().toLowerCase();
+        let computerChoice = computerSelection();
 
 
-function winner(playerChoice, computerPlay) {
-    if (playerChoice == "rock" && computerPlay == "scissors") {
-        winHead.textContent = "You win! Rocks beats scissors"
-        return "You win! Rocks beats scissors"
-    }
-    if (playerChoice == "rock" && computerPlay == "paper") {
-        winHead.textContent = "You lose! Paper beats rock"
-        return "You lose! Paper beats rock"
-    }
-    if (playerChoice == "paper" && computerPlay == "rock") {
-        winHead.textContent = "You win! Paper beats rock"
-        return "You win! Paper beats rock"
-    }
-    if (playerChoice == "paper" && computerPlay == "scissors") {
-        winHead.textContent = "You lose! Scissors beats paper"
-        return "You lose! Scissors beats paper"
-    }
-    if (playerChoice == "scissors" && computerPlay == "rock") {
-        winHead.textContent = "You lose! Rock beats scissors"
-        return "You lose! Rock beats scissors"
-    }
-    if (playerChoice == "scissors" && computerPlay == "paper") {
-        winHead.textContent = "You win!  Scissors beats paper "
-        return "You win!  Scissors beats paper "
-    }
-    if (playerChoice == computerPlay) {
-        winHead.textContent = "It's a tie. No one scores"
-        return "It's a tie. No one Scores"
-    }
-}
+        function computerSelection() {
+            var choices = ['rock', 'paper', 'scissors'];
+            computerSelection = choices[Math.floor(Math.random() * choices.length)];
+            return computerSelection;
+        };
 
-function validateInput(input) {
-    if (input != "paper" && input != "scissors" && input != "rock") {
-        console.log("Wrong input")
-        personPlay()
-    } else {
-        return input
-    }
-}
+        function playerSelection() {
+            playerSelection = prompt('Please pick Rock Paper or Scissors');
+            return playerSelection;
+        };
 
-function endscore(computerScore, playerScore) {
-    if (computerScore > playerScore) {
-        message = "You lost"
-    } else if (computerScore < playerScore) {
-        message = "You won "
-    } else {
-        message = "Its a tie"
-    }
-    winHead.textContent = message + " : " + playerScore + " against " + computerScore
-}
-
-function playGame() {
-    let playerScore = 0
-    let computerScore = 0
-    for (i = 1; i <= 5; i++) {
-        let computerChoice = computerPlay()
-        let personChoice = playerChoice()
-        let checkedChoicePerson = validateInput(personChoice)
-        solution = winner(checkedChoicePerson, computerChoice)
-        console.log(solution)
-        if (solution.includes("win")) {
-            playerScore++;
-            playerScoreHead.textContent = playerScore;
-
-        }
-        if (solution.includes("lose")) {
-            computerScore++
-            compScoreHead.textContent = computerScore;
-        }
-    }
-    endscore(computerScore, playerScore)
-
-}
-playGame()
+        console.log(playerChoice, computerChoice)
+    };
